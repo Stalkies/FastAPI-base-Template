d645f2e7f2cf775338042d1b840e38df745c2785
Oleksandr Bulanov <jakehuasen@gmail.com>
2025-05-20 20:06:45 +0200

    remove typing import

--- fastapi_app/models/tokens.py
+++ fastapi_app/models/tokens.py
@@ -1,15 +1,14 @@
-from typing import TYPE_CHECKING
-from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, Integer, ForeignKey
 from .base import Base
-
-if TYPE_CHECKING:
-    from .users import User
 
 
 class Token(Base):
 
     token: Mapped[str] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), index=True)
-    user: Mapped["User"] = mapped_column(back_populates="tokens")
-    created_at: Mapped[int] = mapped_column(Integer, index=True)
+    user: Mapped["User"] = relationship("User", back_populates="tokens")
+    created_at: Mapped[int] = mapped_column(
+        Integer, index=True, default=int(datetime.now().timestamp())
+    )
