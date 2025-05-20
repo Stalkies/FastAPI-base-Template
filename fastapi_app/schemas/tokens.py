9b4727e180692e9ac90b99c4ad7a0c4946398ab7
Oleksandr Bulanov <jakehuasen@gmail.com>
2025-05-20 21:44:03 +0200

    Schemas: Add tokens schemas

---
+++ fastapi_app/schemas/tokens.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+
+class TokenBase(BaseModel):
+    token: str
+
+
+class TokenCreate(TokenBase):
+    user_id: int
+
+
+class TokenRead(TokenCreate):
+    created_at: str
